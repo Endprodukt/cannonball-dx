b10649074da2e9f79b7283d0cab930e32aa7ea6d
Endprodukt <o.carell@kabelmail.de>
2026-09-08 03:22:24 +0200

    Allow 10 ms engine vibration period

--- src/main/frontend/config.hpp
+++ src/main/frontend/config.hpp
@@ -21,7 +21,7 @@
     static constexpr int BUMPER_VIEW_HEIGHT_LEVELS = 5; \
     static constexpr int ENGINE_VIBRATION_DEFAULT_STRENGTH = 4; \
     static constexpr int ENGINE_PERIOD_DEFAULT_MS = 110; \
-    static constexpr int ENGINE_PERIOD_MIN_MS = 20; \
+    static constexpr int ENGINE_PERIOD_MIN_MS = 10; \
     static constexpr int ENGINE_PERIOD_MAX_MS = 250; \
     static constexpr int ENDLESS_DEFAULT_START_TIME = 80; \
     static constexpr int ENDLESS_DEFAULT_CHECKPOINT_TIME = 55; \
